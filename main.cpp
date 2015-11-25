8af3527ce9174e12f7cf7b6f163fd8ddc623893e
Anna Petrasova <kratochanna@gmail.com>
2015-11-25 17:05:45 -0500

    add interpolation

--- main.cpp
+++ main.cpp
@@ -17,16 +17,8 @@
 #include <pcl/filters/filter.h>
 #include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl/filters/passthrough.h>
-#include <pcl/ModelCoefficients.h>
 #include <pcl/point_types.h>
-#include <pcl/sample_consensus/method_types.h>
-#include <pcl/sample_consensus/model_types.h>
-#include <pcl/segmentation/sac_segmentation.h>
-#include <pcl/segmentation/region_growing.h>
 #include <pcl/filters/extract_indices.h>
-#include <pcl/search/search.h>
-#include <pcl/search/kdtree.h>
-#include <pcl/features/normal_3d.h>
 #include <pcl/surface/mls.h>
 #include <pcl/common/transforms.h>
 #include <pcl/common/angles.h>
@@ -39,9 +31,14 @@ extern "C" {
 #include <grass/raster.h>
 #include <grass/vector.h>
 #include <grass/glocale.h>
+#include <grass/interpf.h>
+#include <grass/gmath.h>
+#include <grass/qtree.h>
+#include <grass/dataquad.h>
 }
 
 #include <stdlib.h>
+
 
 
 template <typename PointT> inline void
@@ -196,7 +193,94 @@ int main(int argc, char **argv)
         Vect_cat_set(Cats, 1, cat);
         Vect_write_line(&Map, GV_POINT, Points, Cats);
     }
+    Vect_rewind(&Map);
+    // interpolation
+    struct interp_params params;
+    static struct Cell_head cellhd;
+    G_get_set_window(&cellhd);
 
+    double ew_res = cellhd.ew_res;
+    double ns_res = cellhd.ns_res;
+    int n_cols = cellhd.cols;
+    int n_rows = cellhd.rows;
+    double x_orig = cellhd.west;
+    double y_orig = cellhd.south;
+    double xm = cellhd.east;
+    double ym = cellhd.north;
+    double dmin, dmax;
+    int npoint = 0;
+    if (ew_res < ns_res)
+        dmin = ew_res / 2;
+    else
+        dmin = ns_res / 2;
+    dmax = dmin * 5;
+    double *az = NULL, *adx = NULL, *ady = NULL, *adxx = NULL, *adyy = NULL,
+        *adxy = NULL;
+    double tension = 30;
+    az = G_alloc_vector(n_cols + 1);
+    if (!az) {
+        G_fatal_error(_("Not enough memory for %s"), "az");
+    }
+    char *tmp = G_tempfile();
+    FILE *Tmp_fd_z = fopen(tmp, "w+");
+    if (!Tmp_fd_z)
+        G_fatal_error(_("Unable to open temporary file <%s>"), tmp);
+
+    struct quaddata *data;
+    struct multfunc *functions;
+    struct multtree *tree;
+    struct multtree *root;
+    struct tree_info *info;
+
+    if ((data =	quad_data_new(x_orig, y_orig, xm, ym, n_rows, n_cols, 0, 40)) == NULL)
+        G_fatal_error(_("Unable to create quaddata"));
+    if ((functions = MT_functions_new(quad_compare, quad_divide_data, quad_add_data,
+                                      quad_intersect, quad_division_check,
+                                      quad_get_points)) == NULL)
+        G_fatal_error(_("Unable to create quadfunc"));
+
+    if ((tree = MT_tree_new(data, NULL, NULL, 0)) == NULL)
+        G_fatal_error(_("Unable to create tree"));
+    root = tree;
+
+    if ((info = MT_tree_info_new(root, functions, dmin, 40)) == NULL)
+        G_fatal_error(_("Unable to create tree info"));
+
+    double xmin, xmax, ymin, ymax, zmin, zmax;
+    IL_init_params_2d(&params, NULL, 1, 1, 1, 80, 40, NULL, n_rows,
+                      n_cols, az, adx, ady, adxx, adyy, adxy, tension, 160,
+                      1, 1, 1, 1, voutput_opt->answer, NULL, NULL, NULL,
+                      NULL, NULL, dmin, x_orig, y_orig, 0, 0,
+                      1, Tmp_fd_z, NULL, NULL, NULL,
+                      NULL, NULL, NULL, NULL, 0, "");
+    IL_init_func_2d(&params, IL_grid_calc_2d, IL_matrix_create,
+                    IL_check_at_points_2d, IL_secpar_loop_2d, IL_crst,
+                    IL_crstg, IL_write_temp_2d);
+    int totsegm = IL_vector_input_data_2d(&params, &Map, 0,
+                                          NULL, NULL,
+                                          info, &xmin, &xmax,
+                                          &ymin, &ymax, &zmin, &zmax, &npoint, &dmax);
+    double ertot, zminac, zmaxac;
+    double gmin, gmax, c1min, c1max, c2min, c2max;
+    double deltx, delty;
+    deltx = xmax - xmin;
+    delty = ymax - ymin;
+    double dnorm =  sqrt((deltx * delty * 80) / npoint);
+    std::cout <<totsegm<< std::endl;
+    std::cout << x_orig<< std::endl;
+    std::cout << y_orig<< std::endl;
+    std::cout << npoint<< std::endl;
+
+    IL_interp_segments_2d(&params, info, info->root, NULL,
+                          zmin, zmax, &zminac, &zmaxac, &gmin, &gmax,
+                          &c1min, &c1max, &c2min, &c2max, &ertot, totsegm,
+                          n_cols, dnorm);
+    G_free_vector(az);
+    int ii = IL_output_2d(&params, &cellhd, zmin, zmax, zminac, zmaxac, c1min,
+                          c1max, c2min, c2max, gmin, gmax, ertot, voutput_opt->answer, dnorm,
+                          0, 1, npoint);
+    fclose(Tmp_fd_z);
+    unlink(tmp);
     k2g.shutDown();
     Vect_build(&Map);
     Vect_close(&Map);
