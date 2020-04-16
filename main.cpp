375530246bc1fda8d430a98e8b2d5f4f4cbdfdb0
Anna Petrasova <kratochanna@gmail.com>
2020-04-16 14:49:59 -0400

    fix flag

--- main.cpp
+++ main.cpp
@@ -641,7 +641,7 @@ int main(int argc, char **argv)
     if (G_parser(argc, argv))
         exit(EXIT_FAILURE);
 
-    if (sensor_info_flag) {
+    if (sensor_info_flag->answer) {
         fprintf(stdout, "sensor=k4a\n");
         return EXIT_SUCCESS;
     }
